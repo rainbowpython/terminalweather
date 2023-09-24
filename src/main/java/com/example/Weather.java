6146683832bb0b44d81a84a7b6ff098432f91b04
rainbowpython <joshble@outlook.com>
2023-09-24 02:58:29 -0700

    uwu

--- src/main/java/com/example/Weather.java
+++ src/main/java/com/example/Weather.java
@@ -12,10 +12,12 @@ public class Weather {
     private String current;
     
     public static final int CURRENT = 1;
-    private int precipitation;
-    private int windSpeed;
-    private double temp;
+    private String precipitation;
+    private String windSpeed;
+    private String temp;
     private String icon;
+    private String humidity;
+    private String windDirection;
     Weather(){
 
     }
@@ -24,13 +26,12 @@ public class Weather {
         .uri(URI.create("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=metric&include=current&key=UCVG62VSNL8D8CJHSR2KXK6L5&contentType=json"))
         .method("GET", HttpRequest.BodyPublishers.noBody()).build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        //System.out.println(response.body());
-        FileWriter file = new FileWriter("weather.txt");
-        file.write(response.body());
         System.out.println(response.body().substring(response.body().indexOf("currentConditions")));
         current = response.body().substring(response.body().indexOf("currentConditions"));
+        temp = current.substring(current.indexOf("temp\":")+6, current.indexOf(",", current.indexOf("temp\":")+1));
+        precipitation = current.substring(current.indexOf("precip\":")+6, current.indexOf(",", current.indexOf("temp\":")+1));
+        System.out.println(temp);
         for (int i = 0; i < response.body().length(); i++) {
-            //System.out.println(response.body());
             
         }
         
@@ -38,5 +39,9 @@ public class Weather {
 
     public String getCurrentWeather(){
         return current;
+    }
+
+    public String getTemp(){
+        return temp;
     }
 }
