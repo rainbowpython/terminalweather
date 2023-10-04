305a9b54292d98e2ccaba85c21c3ff6d61f05ec2
rainbowpython <joshble@outlook.com>
2023-10-03 22:43:08 -0700

    work

--- src/main/java/com/example/Display.java
+++ src/main/java/com/example/Display.java
@@ -2,45 +2,82 @@ package com.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Display {
     
-    File file;
-    Scanner scanner;
+    private File file;
+    private Scanner scanner;
+    private boolean rain;
+    String iconColor;
     public static final String COLOR_RESET = "\u001B[0m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String WHITE = "\u001B[37m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String MAGENTA = "\u001B[35m";
+
+    Calendar calendar;
     Display(Weather weather) throws FileNotFoundException{
         //real image starts 14 spaces in
         file = new File("src\\main\\resources\\" +  weather.getIcon() + ".txt");
         scanner = new Scanner(file);
+        calendar = Calendar.getInstance();
 
         int line = 0;
         int nameLength = 0;
-        String iconColor = "\u001B[34m";
+        rain = (weather.getIcon().equals("rain") || weather.getIcon().equals("snow"));
+        iconColor = WHITE;
 
         if (weather.getIcon().equals("clear-day") || weather.getIcon().equals("clear-night")) {
-            iconColor = "\u001B[33m";
+            iconColor = YELLOW;
         }
-
+         
         while(scanner.hasNextLine()){
             
-            if (weather.getIcon().equals("partly-cloudy-day")) {
+            if ((weather.getIcon().equals("partly-cloudy-day") || weather.getIcon().equals("partly-cloudy-night")) && line < 11) {
                 colorizeIcon();
-            } else {
+            } 
+            else if (rain && line > 14) {
+                System.out.print(CYAN + scanner.nextLine() + COLOR_RESET);
+            }
+            else {
                 System.out.print(iconColor + scanner.nextLine() + COLOR_RESET);
             }
             
-            
+
             if (line == 0) {
-                
                 String name = "Hello " + "\u001B[36m" + System.getProperty("user.name") + COLOR_RESET + "!! :3";
                 System.out.print(name);
                 nameLength = name.length();
             }
 
             if (line == 1) {
-                System.out.print("");
+                System.out.print("Its ");
+                switch (weather.getIcon()){
+                    case "rain":
+                        System.out.print("raining outside");
+                    break;
+
+                    case "cloudy":
+                    case "partly-cloudy-night":
+                    case "partly-cloudy-day":
+                        System.out.print("cloudy outside");
+                    break;
+
+                    case "clear-day":
+                        System.out.print("sunny outside");
+                    break;
+
+                    default:
+                    break;
+                }
             }
             
             if (line == 2) {
@@ -62,10 +99,9 @@ public class Display {
                         System.out.print(c);
                     }
                 }
+                
             }
 
-            
-            
             System.out.println("");
             line++;
         }
@@ -77,10 +113,11 @@ public class Display {
         char[] line =  next.toCharArray();
         for (char c : line) {
             if (c == 'G' || c == 'B') {
-                System.out.print("\u001B[33m" + c + COLOR_RESET);
+                System.out.print(YELLOW + c + COLOR_RESET);
             }
+
             else{
-                System.out.print(c);
+                System.out.print(iconColor + c + COLOR_RESET);
             }
         }
     }
